00a2e3fc165ba9072db3ba7beac57582179ffd8d
Beat Küng <beat-kueng@gmx.net>
2012-06-28 18:46:01 +0200

    c code formatting

--- src/activate.py
+++ src/activate.py
@@ -3,7 +3,9 @@
 import dbus
  
 bus = dbus.SessionBus()
-service = bus.get_object('org.rhythmbox.JumpToWindow', '/org/rhythmbox/JumpToWindow')
-service_func = service.get_dbus_method('dbus_activate', 'org.rhythmbox.JumpToWindow')
+service = bus.get_object('org.rhythmbox.JumpToWindow'
+        , '/org/rhythmbox/JumpToWindow')
+service_func = service.get_dbus_method('dbus_activate'
+        , 'org.rhythmbox.JumpToWindow')
 print service_func("")
 
